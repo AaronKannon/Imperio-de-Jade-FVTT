c374891463eac692c2c9656a8a54f03bcc0b0558
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-07-03 10:18:06 -0300

    Optimizing the Chatbox code by creating a HBS file and injecting into the sheets

--- module/templates.js
+++ module/templates.js
@@ -1,21 +1,22 @@
 /**
-* Define a set of template paths to pre-load
-* Pre-loaded templates are compiled and cached for fast access when rendering
-* @return {Promise}
-*/
+ * Define a set of template paths to pre-load
+ * Pre-loaded templates are compiled and cached for fast access when rendering
+ * @return {Promise}
+ */
 export const preloadHandlebarsTemplates = async function() {
     console.log("IDJ | Carregando os templates parciais");
-	return loadTemplates([
+    return loadTemplates([
 
-		// Shared Partials
+        // Shared Partials
 
-		// Actor Sheet Partials
+        // Actor Sheet Partials
 
 
-		// Item Sheet Partials
-		"systems/imperiodejade/templates/item/parts/item-header.hbs",
-		"systems/imperiodejade/templates/item/parts/item-description.hbs",
-		"systems/imperiodejade/templates/item/parts/item-ativacao.hbs",
-		"systems/imperiodejade/templates/item/parts/item-resistencia.hbs"
-	]);
+        // Item Sheet Partials
+        "systems/imperiodejade/templates/item/parts/item-header.hbs",
+        "systems/imperiodejade/templates/item/parts/item-description.hbs",
+        "systems/imperiodejade/templates/item/parts/item-ativacao.hbs",
+        "systems/imperiodejade/templates/item/parts/item-resistencia.hbs",
+        "systems/imperiodejade/templates/item/parts/item-chatbox.hbs"
+    ]);
 };
